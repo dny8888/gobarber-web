819bc0d3fb58c767d0e5bc0be543e0d6aab36c17
dny8888 <dny8888@gmail.com>
2020-09-06 18:16:29 -0300

    feat: signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './page/SignIn/index';
+import SignUp from './page/SignUp/index';
 
 const App: React.FC = () => (
     <>
         <GlobalStyle />
-        <SignIn />
+        <SignUp />
     </>
 );
 
