f204e45731570eda501db5182b33c36b8913914d
dny8888 <dny8888@gmail.com>
2020-09-07 16:22:01 -0300

    refac: inputs handlings

--- src/page/SignUp/index.tsx
+++ src/page/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 
 import Input from '../../components/Input';
@@ -7,29 +8,41 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="Gobarber" />
-            <form action="">
-                <h1>Faça seu cadastro</h1>
-                <Input name="nome" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    type="password"
-                    placeholder="Senha"
-                />
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="login">
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-    </Container>
-);
+interface SignUpProps {
+    nome: string;
+    email: string;
+    password: string;
+}
+
+const SignUp: React.FC = () => {
+    function handleSubmit(data: SignUpProps): void {
+        console.log(data);
+    }
+
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="Gobarber" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="login">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
