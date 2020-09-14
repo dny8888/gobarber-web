769e18f75bccd271885731974101f9241cca6f74
dny8888 <dny8888@gmail.com>
2020-09-14 00:11:50 -0300

    feat:  added AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './page/SignIn/index';
 // import SignUp from './page/SignUp/index';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
     <>
         <GlobalStyle />
-        <SignIn />
+        <AuthProvider>
+            <SignIn />
+        </AuthProvider>
     </>
 );
 
