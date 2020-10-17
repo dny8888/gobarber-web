165afccce83ab8860003569e660ed20265d167a5
dny8888 <dny8888@gmail.com>
2020-10-16 22:53:55 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
-import SignIn from './page/SignIn/index';
-// import SignUp from './page/SignUp/index';
+
+import Routes from './routes';
+
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
-    <>
+    <Router>
         <AppProvider>
-            <SignIn />
+            <Routes />
         </AppProvider>
         <GlobalStyle />
-    </>
+    </Router>
 );
 
 export default App;
